2d55badffa75df83b4ca21e3198d178f269b625f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-02-24 11:41:37 +0100

    wip: routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
-import { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./sass/App.scss";
-import Navigation from "./components/Navigation/Navigation";
-import Heading from "./components/Heading";
-import ProfilePhoto from "./components/ProfilePhoto";
-import RepoList from "./components/RepoList";
+import Home from "./Home";
+import About from "./About";
 
 export default function App({ username }) {
-  const [myGitHubData, setMyGitHubData] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        const apiData = response.json();
-        return apiData;
-      })
-      .then((apiData) => setMyGitHubData(apiData))
-      .catch((error) => console.log(error));
-  }, [username]);
-
-  if (myGitHubData) {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <ProfilePhoto
-            avatarUrl={myGitHubData.avatar_url}
-            avatarAlt={myGitHubData.name}
-          />
-          <Heading name={myGitHubData.name} size="2" color="grey" />
-        </header>
-        <Navigation />
-        <main>
-          <RepoList username={username} />
-        </main>
-      </div>
-    );
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">NO DATA SORRY</header>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Home username={username} />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </Switch>
   );
 }
