7236e76be6f5981902bd76b8a37c052ee70e4bd7
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-02-25 09:40:45 +0100

    wip: bootstrap

--- src/About.js
+++ src/About.js
@@ -1,8 +1,10 @@
 import Navigation from "./components/Navigation/Navigation";
-export default function About() {
+
+export default function About({ username }) {
   return (
     <>
       <h1>About</h1>
+      <p>{username}</p>
       <Navigation />
     </>
   );
--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Switch, Route } from "react-router-dom";
+
 import "./sass/App.scss";
 import Home from "./Home";
 import About from "./About";
+import Contact from "./Contact";
 
 export default function App({ username }) {
   return (
     <Switch>
-      <Route path="/" exact>
-        <Home username={username} />
-      </Route>
+      <Route path="/" exact component={() => <Home username={username} />} />
       <Route path="/about">
         <About username={username} />
+      </Route>
+      <Route path="/contact">
+        <Contact />
       </Route>
     </Switch>
   );
